2b30ebff21bc37aa33a9a2e4b6af9e5c5640753b
Jennifer Zhang <jenniferjuliazhang@gmail.com>
2017-02-25 11:36:16 -0500

    Routes for uploading photos

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='squiggly',
+    packages=['squiggly'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
---
+++ squiggly/views.py
@@ -0,0 +1,41 @@
+import os
+from squiggly import app
+from flask import Flask, request, redirect, render_template, send_from_directory, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '../photos'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            return vision(file)
+    return render_template('upload.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+def vision(file):
+    return file.filename
