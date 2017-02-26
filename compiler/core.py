ae4d261e1134beffb603cb541e33b13cf9283065
Anthony Liu <anthonyscottliu@gmail.com>
2017-02-25 21:22:25 -0500

    Adds keywords.

--- compiler/core.py
+++ compiler/core.py
@@ -2,6 +2,23 @@
 Squiggly compiler module
 """
 
+keywords = {
+    'for': 'FOR',
+    'forward': 'FWD',
+    'turn': 'TRN',
+    'chocolate': 'CHC',
+    'eat': 'EAT',
+    'banana': 'BAN'
+}
+id_to_keyword = {
+    'RBG': keywords['for'],
+    'BRB': keywords['forward'],
+    'RGR': keywords['turn'],
+    'BRG': keywords['chocolate'],
+    'BGR': keywords['eat'],
+    'RGB': keywords['banana']
+}
+
 # given a list of block locations and block IDs, return a list of
 # commands to execute
 def compile(blocks):
@@ -12,9 +29,9 @@ def compile(blocks):
 
 # organize the blocks in lines
 def lineate(blocks):
-    return []
+    return blocks
 
 
 # simulate the lines to get an explicit list of commands
 def simulate(lines):
-    return []
+    return lines
